ea2ae39e64def4480bceb34d9fdfe5773a7b65b6
costel.istrate <costel.istrate@orange.com>
2024-02-26 11:01:28 +0200

    commit

---
+++ src/main/java/org/ols/qaprep/QaPrepApplication.java
@@ -0,0 +1,13 @@
+package org.ols.qaprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaPrepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ols/qaprep/QaPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ols.qaprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
